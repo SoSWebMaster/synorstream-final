253e91acf67ebe1145ab29c8b5ac8e004bc933ef
SoSWebMaster <developer@syncorstream.com>
2024-10-02 15:28:59 -0400

    added loading on plan change

--- src/components/dashboad2/menus2/updatePlan/updatePlan.tsx
+++ src/components/dashboad2/menus2/updatePlan/updatePlan.tsx
@@ -7,6 +7,7 @@ import {
    DialogActions,
    DialogContent,
    DialogTitle,
+   CircularProgress,
 } from "@mui/material";
 import { endPoints } from "../../../../services/constants/endPoint";
 import useAxios from "../../../../services/axiosConfig/axiosConfig";
@@ -23,6 +24,7 @@ const UpdatePlan2 = () => {
    const [open, setOpen] = useState<boolean>(false); // Modal open state
    const [selectedPlanId, setSelectedPlanId] = useState<number | null>(null); // Selected plan ID for confirmation
    const [message, setMessage] = useState<any>();
+   const [loading, setLoading] = useState<boolean>(false); // Loading state
    const navigate = useNavigate();
    const axiosInstance = useAxios();
 
@@ -32,6 +34,7 @@ const UpdatePlan2 = () => {
 
    const fetchPricingData = async () => {
       try {
+         setLoading(true); // Start loading
          const response = await axiosInstance.get(endPoints.selected_plan);
          if (response?.data) {
             const data = response?.data;
@@ -40,6 +43,8 @@ const UpdatePlan2 = () => {
          }
       } catch (error) {
          toast.error(`${error}`);
+      } finally {
+         setLoading(false); // Stop loading after data is fetched
       }
    };
 
@@ -52,20 +57,15 @@ const UpdatePlan2 = () => {
       }
    };
 
-   //  const handleOpenModal = (plan_id: number) => {
-   //     setSelectedPlanId(plan_id);
-   //     setOpen(true);
-   //  };
-
    const handleCloseModal = () => {
       setOpen(false);
       setSelectedPlanId(null);
    };
 
    const showConfirmModal = async (plan_id: number) => {
-      // if (selectedPlanId !== null) {
       try {
          setSelectedPlanId(plan_id);
+         setLoading(true); // Start loading
          const response = await axiosInstance.post(endPoints.cal_pro_rate_amt, {
             play_type: viewMode,
             sel_plan: plan_id,
@@ -74,17 +74,17 @@ const UpdatePlan2 = () => {
             const data = response?.data;
             setOpen(true);
             setMessage(data.msg);
-            // Handle response data here
          }
       } catch (error) {
          toast.error(`${error}`);
+      } finally {
+         setLoading(false); // Stop loading
       }
-      // }
    };
 
    const handleConfirm = async () => {
-      // if (selectedPlanId !== null) {
       try {
+         setLoading(true); // Start loading
          const response = await axiosInstance.post(endPoints.switch_plan, {
             req: selectedPlanId,
             play_type: viewMode,
@@ -94,13 +94,16 @@ const UpdatePlan2 = () => {
             const data = response?.data;
             setOpen(false);
             toast.success(data.msg);
-            // Handle response data here
+            // Re-fetch updated plan data after successful update
+            await fetchPricingData(); // Refetch the data to show new changes
          }
       } catch (error) {
          toast.error(`${error}`);
+      } finally {
+         setLoading(false); // Stop loading
       }
-      // }
    };
+
 
    return (
       <>
@@ -108,114 +111,126 @@ const UpdatePlan2 = () => {
             <div className="m-12">
                <p className="text-[28px]">My Plan</p>
                <p className="text-[24px] mt-6">Choose Plan</p>
-               <ToggleButtonGroup
-                  value={viewMode}
-                  exclusive
-                  onChange={handleToggleChange}
-                  aria-label="View Mode"
-                  sx={{
-                     mb: 4,
-                     border: "1px solid #FB8A2E",
-                     borderRadius: "8px",
-                     "& .Mui-selected": {
-                        backgroundColor: "#FB8A2E",
-                        // color: "#fff",
-                        // "&:hover": {
-                        //    backgroundColor: "#FB8A2E",
-                        // },
-                     },
-                     "& .MuiToggleButton-root": {
-                        border: "1px solid #FB8A2E",
-                        // color: "#FB8A2E",
-                        // "&:hover": {
-                        //    backgroundColor: "#FFE4E1",
-                        // },
-                     },
-                  }}
-               >
-                  <ToggleButton
-                     value={0}
-                     aria-label="Monthly"
-                     sx={{ color: "white" }}
-                  >
-                     Monthly
-                  </ToggleButton>
-                  <ToggleButton
-                     value={1}
-                     aria-label="Yearly"
-                     sx={{ color: "white" }}
-                  >
-                     Yearly
-                  </ToggleButton>
-               </ToggleButtonGroup>
-               {pricingData.length > 0 &&
-                  pricingData?.map((item) => (
-                     <div
-                        key={item.id}
-                        className="flex justify-between border gap-7 w-full border-[#FB8A2E] h-32 mt-8 rounded-3xl p-7 justify-between items-center"
+
+               {loading ? (
+                  <div className="flex justify-center">
+                     <CircularProgress color="primary" />
+                  </div>
+               ) : (
+                  <>
+                     <ToggleButtonGroup
+                        value={viewMode}
+                        exclusive
+                        onChange={handleToggleChange}
+                        aria-label="View Mode"
+                        sx={{
+                           mb: 4,
+                           border: "1px solid #FB8A2E",
+                           borderRadius: "8px",
+                           "& .Mui-selected": {
+                              backgroundColor: "#FB8A2E",
+                           },
+                           "& .MuiToggleButton-root": {
+                              border: "1px solid #FB8A2E",
+                           },
+                        }}
                      >
+                        <ToggleButton
+                           value={0}
+                           aria-label="Monthly"
+                           sx={{ color: "white" }}
+                           disabled={loading} // Disable during loading
+                        >
+                           Monthly
+                        </ToggleButton>
+                        <ToggleButton
+                           value={1}
+                           aria-label="Yearly"
+                           sx={{ color: "white" }}
+                           disabled={loading} // Disable during loading
+                        >
+                           Yearly
+                        </ToggleButton>
+                     </ToggleButtonGroup>
+
+                     {pricingData.length > 0 &&
+                        pricingData?.map((item) => (
+                           <div
+                              key={item.id}
+                              className="flex justify-between border gap-7 w-full border-[#FB8A2E] h-32 mt-8 rounded-3xl p-7 justify-between items-center"
+                           >
+                              <div className="flex justify-between w-full">
+                                 <div>
+                                    <p className="text-[28px] font-semibold">
+                                       {item?.plan_name}
+                                    </p>
+                                    <p className="text-[18px] ">
+                                       {item?.sub_heading}
+                                    </p>
+                                 </div>
+                                 <div>
+                                    {viewMode === 0 ? (
+                                       <p className="text-[28px] font-semibold">
+                                          {"$" + item?.month_price + "/ month"}
+                                       </p>
+                                    ) : (
+                                       <p className="text-[28px] font-semibold">
+                                          {"$" + item?.year_price + "/ year"}
+                                       </p>
+                                    )}
+                                    {viewMode === 0 ? (
+                                       <p className="text-[18px] text-[#BBBBBB]">{`or billed at $${item?.year_price}/yearly ( ${item?.free_months_text})`}</p>
+                                    ) : (
+                                       <p className="text-[18px] text-[#BBBBBB]">{`Includes ${item?.free_months_text}`}</p>
+                                    )}
+                                 </div>
+                              </div>
+                              <div>
+                                 <Button
+                                    className="!bg-[#FB8A2E] !text-white !w-48 !h-12 !rounded-3xl"
+                                    onClick={() => showConfirmModal(item?.id)}
+                                    disabled={loading} // Disable during loading
+                                 >
+                                    {loading ? (
+                                       <CircularProgress
+                                          color="inherit"
+                                          size={24}
+                                       />
+                                    ) : (
+                                       "Select"
+                                    )}
+                                 </Button>
+                              </div>
+                           </div>
+                        ))}
+
+                     <div className="flex justify-between border gap-7 w-full border-[#FB8A2E] h-32 mt-8 rounded-3xl p-7 justify-between items-center">
                         <div className="flex justify-between w-full">
                            <div>
                               <p className="text-[28px] font-semibold">
-                                 {item?.plan_name}
+                                 {currentPlan?.plan_name}
                               </p>
                               <p className="text-[18px] ">
-                                 {item?.sub_heading}
+                                 {currentPlan?.sub_heading}
                               </p>
                            </div>
                            <div>
-                              {/* Show pricing according to viewMode */}
-                              {viewMode === 0 ? (
-                                 <p className="text-[28px] font-semibold">
-                                    {"$" + item?.month_price + "/ month"}
-                                 </p>
-                              ) : (
-                                 <p className="text-[28px] font-semibold">
-                                    {"$" + item?.year_price + "/ year"}
-                                 </p>
-                              )}
-                              {viewMode === 0 ? (
-                                 <p className="text-[18px] text-[#BBBBBB]">{`or billed at $${item?.year_price}/yearly ( ${item?.free_months_text})`}</p>
-                              ) : (
-                                 <p className="text-[18px] text-[#BBBBBB]">{`Includes ${item?.free_months_text}`}</p>
-                              )}
+                              <p className="text-[28px] font-semibold">
+                                 {"$" +
+                                    currentPlan?.current_plan_selected_price +
+                                    "/" +
+                                    currentPlan?.period}
+                              </p>
                            </div>
                         </div>
                         <div>
-                           <Button
-                              className="!bg-[#FB8A2E] !text-white !w-48 !h-12 !rounded-3xl"
-                              onClick={() => showConfirmModal(item?.id)}
-                           >
-                              Select
-                           </Button>
+                           <p className="border !border-[#FB8A2E] !text-white !w-48 !h-12 rounded-lg text-center flex justify-center items-center ">
+                              Activated
+                           </p>
                         </div>
                      </div>
-                  ))}
-               <div className="flex justify-between border gap-7 w-full border-[#FB8A2E] h-32 mt-8 rounded-3xl p-7 justify-between items-center">
-                  <div className="flex justify-between w-full">
-                     <div>
-                        <p className="text-[28px] font-semibold">
-                           {currentPlan?.plan_name}
-                        </p>
-                        <p className="text-[18px] ">
-                           {currentPlan?.sub_heading}
-                        </p>
-                     </div>
-                     <div>
-                        <p className="text-[28px] font-semibold">
-                           {"$" +
-                              currentPlan?.current_plan_selected_price +
-                              "/" +
-                              currentPlan?.period}
-                        </p>
-                     </div>
-                  </div>
-                  <div>
-                     <p className="border !border-[#FB8A2E] !text-white !w-48 !h-12 rounded-lg text-center flex justify-center items-center ">
-                        Activated
-                     </p>
-                  </div>
-               </div>
+                  </>
+               )}
             </div>
 
             {/* Confirmation Modal */}
@@ -245,8 +260,13 @@ const UpdatePlan2 = () => {
                   <Button
                      onClick={handleConfirm}
                      className="!bg-[#FB8A2E] !text-white !p-2 !px-4 !rounded-3xl"
+                     disabled={loading} // Disable during loading
                   >
-                     Confirm
+                     {loading ? (
+                        <CircularProgress color="inherit" size={24} />
+                     ) : (
+                        "Confirm"
+                     )}
                   </Button>
                </DialogActions>
             </Dialog>
